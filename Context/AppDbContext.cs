34358413c9c8dcdcb2b5999c7e79540da954f334
Vanessa-Bertoldo <vanessa.bert311@gmail.com>
2024-02-08 19:39:08 -0300

    insert context

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using lanchonete.Models;
+using Microsoft.EntityFrameworkCore;
+namespace lanchonete.Context
+{
+    public class AppDbContext :DbContext
+    {
+        //Define as classes do dominio que se deseja mapear
+        //As strings de conexao com o banco sao definidas no arquivo appsettings.json
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set;}
+    }
+}
--- obj/Debug/net8.0/lanchonete.AssemblyInfo.cs
+++ obj/Debug/net8.0/lanchonete.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lanchonete")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a845c4c03baf4e522b9a65bf2f3dd88d3277f2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72acd09f40ed93c4d1cef9695780df7a615803fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("lanchonete")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lanchonete")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
