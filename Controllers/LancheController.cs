a990b6d82bf2fba09b1c31e3f7a2589395d78d34
Vanessa-Bertoldo <vanessa.bert311@gmail.com>
2024-02-21 21:38:14 -0300

    test ViewData and insert methods view model

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using lanchonete.Repositories.Interfaces;
+using lanchonete.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lanchonete.Controllers
@@ -13,8 +14,22 @@ namespace lanchonete.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            //O tempo de vida do ViewData é o request. É mais rápida que as demais
+            ViewData["Titulo"] = "Todos os Lanches";
+            ViewData["Data"] = DateTime.Now;
+            //ViewBag é uma propriedade dinâmica, armazena dados no formato chave/valor e não requer a conversão de tipos
+            //Também possui o tempo de vida request
+            //ViewBag.Chave = "valor";
+
+            //TempData tranfere dados entre controller e view e metodos actions. Armazenas valores formato chave/valor.
+            //Armazena os dados temporariamente e os remove automaticamente após recuperar um valor. Exige a conversão de tipos
+            //TempData["Chave"] = "valor";
+
+            //var lanches = _lancheRepository.Lanches;
+            LanchesListViewModel model = new LanchesListViewModel();
+            model.Lanches = _lancheRepository.Lanches;
+            model.CategoriaAtual = "Categoria";
+            return View(model);
         }
     }
 }
