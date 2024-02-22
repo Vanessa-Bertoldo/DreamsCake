31fea9a9977e630fc0b3fdbbaf0b64d98acce5fc
Vanessa-Bertoldo <vanessa.bert311@gmail.com>
2024-02-21 21:34:02 -0300

    insert ViewModel for Lanches List

---
+++ ViewModel/LanchesListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using lanchonete.Models;
+
+namespace lanchonete.ViewModel
+{
+    public class LanchesListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
